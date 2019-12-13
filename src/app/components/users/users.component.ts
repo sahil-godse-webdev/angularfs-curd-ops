62f88314020c0664df7c7680869606b11bfadc15
sahil-godse-webdev <sahilgodse1998@gmail.com>
2019-12-13 06:31:15 +0530

    ready with delete-user operation

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user';
+//import { userInfo } from 'os';
 
 @Component({
   selector: 'app-users',
@@ -15,6 +16,10 @@ export class UsersComponent implements OnInit {
     this.userService.getUsers().subscribe(users=>
       //console.log(users));
       this.users = users);
+  }
+
+  deleteUser(event, user){
+    this.userService.deleteUser(user);
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,32 @@ export class UserService {
 
   usersCollection: AngularFirestoreCollection<User>;
   users: Observable<User[]>;
+  userDoc: AngularFirestoreDocument<User>;
+
   constructor( public afs: AngularFirestore) { 
-    this.usersCollection= this.afs.collection('users',ref=> ref.orderBy('name','asc'));
-    this.users = this.usersCollection.valueChanges();
+    
+    this.usersCollection = afs.collection<User>('users',ref=> ref.orderBy('name','asc'));
+    // .snapshotChanges() returns a DocumentChangeAction[], which contains
+    // a lot of information about "what happened" with each change. If you want to
+    // get the data and the id use the map operator.
+    this.users = this.usersCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as User;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+    
+    /*this.usersCollection= this.afs.collection('users',ref=> ref.orderBy('name','asc'));
+    this.users= this.afs.collection('users').snapshotChanges().map(changes=>{
+      return changes.map(a=> {
+        const data= a.payload.doc.data() as User;
+        data.id= a.payload.doc.id;
+        return data;
+      })
+    })*/
   }
+  
 
   getUsers(){
     return this.users;
@@ -21,5 +44,11 @@ export class UserService {
 
   addUser(user: User){
     this.usersCollection.add(user);
+  }
+
+  deleteUser(user: User){
+    console.log(user.name);
+    this.userDoc= this.afs.doc(`users/${user.id}`);
+    console.log(this.userDoc.delete());
   }
 }
